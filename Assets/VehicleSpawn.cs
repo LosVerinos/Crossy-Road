1fcec63476c8b967bd084a2c5ed7702e525a02b3
Robin <robin.bechlem@gmail.com>
2024-03-06 10:44:05 +0100

    Ajout des voitures

---
+++ Assets/VehicleSpawn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject vehicle;
+    [SerializeField] private Transform SpawnPos;
+    [SerializeField] private float minSeparationTime;
+    [SerializeField] private float maxSeparationTime;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartCoroutine(SpawnVehicle());
+        
+    }
+
+    private IEnumerator SpawnVehicle()
+    {
+        yield return new WaitForSeconds(Random.Range(minSeparationTime, maxSeparationTime));
+        Instantiate(vehicle, SpawnPos.position, Quaternion.identity);
+    }
+
+}
