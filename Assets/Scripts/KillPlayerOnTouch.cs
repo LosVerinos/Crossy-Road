4463301c2b6683596b7926978b7d22649983d5d9
Alexandre <aofferle@cfai-formation.fr>
2024-03-06 10:15:19 +0100

    Resolved: Quand le joueur arrive dans de l'eau, il est Destroy. On pourra ajouter le script pour les vehicules aussi

---
+++ Assets/Scripts/KillPlayerOnTouch.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayerOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+       if (other.gameObject.CompareTag("Player"))
+       {
+           Destroy(other.gameObject);
+           
+       }
+    }
+}
