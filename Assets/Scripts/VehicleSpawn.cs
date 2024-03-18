6a3efa000e3a708184dcdc2de8100567aa76533f
Pichon Sebastien <arthox.mail@gmail.com>
2024-03-18 11:42:28 +0100

    Adding random vehicule choice on road

--- Assets/Scripts/VehicleSpawn.cs
+++ Assets/Scripts/VehicleSpawn.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
 public class VehicleSpawn : MonoBehaviour
 {
-    [SerializeField] private GameObject vehicle;
-    [SerializeField] private Transform SpawnPos;
+    [SerializeField] private List<GameObject> vehicles; // Liste des véhicules possibles
+    [SerializeField] private Transform spawnPos;
     [SerializeField] private float minSeparationTime;
     [SerializeField] private float maxSeparationTime;
 
@@ -13,7 +14,6 @@ public class VehicleSpawn : MonoBehaviour
     private void Start()
     {
         StartCoroutine(SpawnVehicle());
-        
     }
 
     private IEnumerator SpawnVehicle()
@@ -21,7 +21,39 @@ public class VehicleSpawn : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(minSeparationTime, maxSeparationTime));
-            Instantiate(vehicle, SpawnPos.position, Quaternion.identity);
+            // Choix aléatoire d'un véhicule dans la liste
+            int randomIndex = Random.Range(0, vehicles.Count);
+            GameObject selectedVehicle = vehicles[randomIndex];
+            // Instanciation du véhicule choisi à la position de spawn
+            Instantiate(selectedVehicle, spawnPos.position, Quaternion.identity);
+        }
+    }
+}*/
+
+
+public class VehicleSpawn : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> vehicles;
+    [SerializeField] private Transform SpawnPos;
+    [SerializeField] private float minSeparationTime;
+    [SerializeField] private float maxSeparationTime;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // Choix aléatoire d'un véhicule dans la liste
+        int randomIndex = Random.Range(0, vehicles.Count);
+        GameObject selectedVehicle = vehicles[randomIndex];
+        StartCoroutine(SpawnVehicle(selectedVehicle));
+        
+    }
+
+    private IEnumerator SpawnVehicle(GameObject selectedVehicle)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minSeparationTime, maxSeparationTime));
+            Instantiate(selectedVehicle, SpawnPos.position, Quaternion.identity);
         }
         
     }
