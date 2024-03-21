5800699b96ec9ba6b23dfa4ca75d6a3c8f87ad08
Pichon Sebastien <arthox.mail@gmail.com>
2024-03-21 21:51:10 +0100

    Forgot to add all files

---
+++ Assets/Scripts/LogSpawnScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LogSpawnScript : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> logs;
+    [SerializeField] private Transform SpawnPos;
+    [SerializeField] private float minSeparationTime;
+    [SerializeField] private float maxSeparationTime;
+    private float speed;
+    int randomIndex;
+    GameObject selectedLog;
+    private void Start()
+    {
+        // Choix aléatoire d'un véhicule dans la liste
+
+        speed = Random.Range(1.5f, 3f);
+        
+        StartCoroutine(SpawnLog());
+        
+    }
+
+    private IEnumerator SpawnLog()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minSeparationTime, maxSeparationTime));
+            randomIndex = Random.Range(0, logs.Count);
+            selectedLog = logs[randomIndex];
+            Instantiate(selectedLog, SpawnPos.position, Quaternion.identity);
+            selectedLog.GetComponent<LogScript>().SetSpeed(speed);
+        }
+        
+    }
+}
