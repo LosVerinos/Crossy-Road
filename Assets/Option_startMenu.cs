b0955efb20bf72f537bd1f7c1a6386f357a305ab
Robin <robin.bechlem@gmail.com>
2024-03-24 17:59:32 +0100

    ajout des différents clicks sur bouton, et du shop

---
+++ Assets/Option_startMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option_startMenu : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OnClick()
+    {
+        Panel.SetActive(true);
+    }
+}
