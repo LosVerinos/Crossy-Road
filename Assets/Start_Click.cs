658a129e77d41a9dd59386f3273212a5410637f4
Robin <robin.bechlem@gmail.com>
2024-04-03 13:41:33 +0200

    ajout de modification sur le menu graphique

--- Assets/Start_Click.cs
+++ Assets/Start_Click.cs
@@ -7,9 +7,15 @@ public class Start_Click : MonoBehaviour
 {
 
     public GameObject Panel;
+    public GameObject Panel2;
+    public GameObject Panel3;
+    public GameObject Panel4;
 
     public void OnButtonClick()
     {
         Panel.SetActive(false);
+        Panel2.SetActive(true);
+        Panel3.SetActive(true);
+        Panel4.SetActive(false);
     }
 }
