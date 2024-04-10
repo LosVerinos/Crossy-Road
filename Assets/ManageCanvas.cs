ececf01917635eccd8fc0edb3c490e8a28d5f6c3
Robin <robin.bechlem@gmail.com>
2024-04-10 19:33:36 +0200

    ajout des interactions sur le menu pause

--- Assets/ManageCanvas.cs
+++ Assets/ManageCanvas.cs
@@ -19,8 +19,6 @@ public class ManageCanvas : MonoBehaviour
     public GameObject scorePanel;
 
 
-    public Animator animator;
-
     public void Start()
     {
         Debug.Log("scrip started well");
@@ -53,106 +51,6 @@ public class ManageCanvas : MonoBehaviour
 
         Time.timeScale = 1;
     }
-
-    public void StartButton_click()
-    {
-        paramButtonPanel.SetActive(false);
-        startMenuPanel.SetActive(false);
-    }
-
-    public void IdentityButton_click()
-    {
-        startMenuPanel.SetActive(false);
-        paramButtonPanel.SetActive(false);
-        coinsPanel.SetActive(false);
-
-        identityPanel.SetActive(true);
-    }
-
-    public void ShopButton_click()
-    {
-        startMenuPanel.SetActive(false);
-        paramButtonPanel.SetActive(false);
-
-        shopPanel.SetActive(true);
-    }
-
-    public void ClassementButton_click()
-    {
-        startMenuPanel.SetActive(false);
-        paramButtonPanel.SetActive(false);
-        coinsPanel.SetActive(false);
-
-        leaderBoardPanel.SetActive(true);
-    }
-
-    public void ParamButton_click()
-    {
-        startMenuPanel.SetActive(false);
-        paramButtonPanel.SetActive(false);
-        coinsPanel.SetActive(false);
-
-        paramPanel.SetActive(true);
-    }
-
-    public void PauseButton_click()
-    {
-        pausePanel.SetActive(true);
-    }
-
-    public void RightButton_click()
-    {
-        leaderBoardPanel.SetActive(false);
-
-        startMenuPanel.SetActive(true);
-        coinsPanel.SetActive(true);
-        scorePanel.SetActive(true);
-    }
-
-    public void UpperButton_click()
-    {
-        //identityPanel.SetActive(false);
-
-        startMenuPanel.SetActive(true);
-        coinsPanel.SetActive(true);
-        scorePanel.SetActive(true);
-    }
-
-    public void LeftButton_click()
-    {
-        shopPanel.SetActive(false);
-
-        scorePanel.SetActive(true);
-        startMenuPanel.SetActive(true);
-    }
-
-    public void MenuFromParamButton_click()
-    {
-        paramPanel.SetActive(false);
-
-        startMenuPanel.SetActive(true);
-        coinsPanel.SetActive(true);
-        paramButtonPanel.SetActive(true);
-
-
-    }
-
-    public void PlayAnimation(string animationName)
-    {
-        if (animator != null)
-        {
-            animator.Play(animationName);
-        }
-        else
-        {
-            Debug.LogWarning("L'Animator n'est pas attribué au script.");
-        }
-    }
-
-
-
-
-
 
     public void QuiteGame()
     {
