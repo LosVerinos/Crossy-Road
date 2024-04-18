a6f275295b2598a6085d7a85bfb684dae44deae8
Robin <robin.bechlem@gmail.com>
2024-04-18 18:07:09 +0200

    ajout du coins et placement aléatoire des coins

--- Assets/Scripts/GlobalVariables.cs
+++ Assets/Scripts/GlobalVariables.cs
@@ -6,11 +6,15 @@ public class GlobalVariables : MonoBehaviour
 {
     public static bool isPlayerKilled = false;
     public static bool run = false;
+    public static int coins;
     public static PlayerScript Player { get; set; }
 
     void Start()
     {
         run = false;
+
+        PlayerPrefs.SetInt("Coins", coins);
+
     }
 
     // Update is called once per frame
