bc92cf1cd438b608d46725dbd0d82dbeca1b0d90
Pichon Sebastien <arthox.mail@gmail.com>
2024-05-03 15:35:46 +0200

    Finishing animation

--- Assets/Scripts/EagleScript.cs
+++ Assets/Scripts/EagleScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class EagleScript : MonoBehaviour
 {
@@ -8,9 +9,6 @@ public class EagleScript : MonoBehaviour
     [SerializeField] GameObject eaglePrebaf;
     private Animator _animator;
     private static readonly int Catch = Animator.StringToHash("catch");
-    public bool isCatching = false;
-
-    // private static readonly int EagleIdle = Animator.StringToHash("DefaultEagle");
 
     // Start is called before the first frame update
     void Start()
@@ -19,17 +17,19 @@ public class EagleScript : MonoBehaviour
     }
 
     public void CatchPlayer(){
-        isCatching = true;
+
         _animator.SetTrigger(Catch);
     }
     
     public void GrabPlayer(){
+        transform.parent.GetComponent<FolowPlayer>().enabled = false;
+        player.GetComponent<BoxCollider>().enabled = false;
+        player.GetComponent<Rigidbody>().useGravity = false;
         player.transform.position = eaglePrebaf.transform.position;
         player.transform.parent = eaglePrebaf.transform;
     }
     
     public void EndCatch(){
-        player.GetComponent<PlayerScript>().KillPlayer();
-        isCatching = false;
+        player.GetComponent<PlayerScript>().KillPlayer(); 
     }
 }
