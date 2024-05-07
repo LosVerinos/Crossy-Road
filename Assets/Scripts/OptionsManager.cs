9ae6ade4c61c30c5d893cca1a8f1f85a6f666e2a
Robin <robin.bechlem@gmail.com>
2024-05-07 15:11:48 +0200

    retirer la résolution et mise en player pref du volume sonore

--- Assets/Scripts/OptionsManager.cs
+++ Assets/Scripts/OptionsManager.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class OptionsManager : MonoBehaviour
 {
-    public Dropdown DResolution;
     public AudioSource audioSrc;
     public Slider sld;
     public Text txtVolume;
@@ -13,7 +12,7 @@ public class OptionsManager : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        InitializeResolutionsDropdown();
+        initSliderChange();
         SliderChange();
     }
 
@@ -23,40 +22,22 @@ public class OptionsManager : MonoBehaviour
         
     }
 
-    public void SetResolution()
-    {
-        Resolution selectedResolution = Screen.resolutions[DResolution.value];
-        Screen.SetResolution(selectedResolution.width, selectedResolution.height, true);
-    }
 
     public void SliderChange()
     {
         audioSrc.volume = sld.value;
         txtVolume.text = "Volume : " + (sld.value * 100).ToString("00") + "%";
+
+        PlayerPrefs.SetFloat("volume", sld.value);
+        PlayerPrefs.Save();
     }
 
-    private void InitializeResolutionsDropdown()
+    private void initSliderChange()
     {
-
-        DResolution.ClearOptions();
-
-        List<string> resolutionOptions = new List<string>();
-
-        foreach (Resolution resolution in Screen.resolutions)
+        if(PlayerPrefs.GetFloat("volume")!=null)
         {
-            string resolutionString = $"{resolution.width} x {resolution.height}";
-            if (!resolutionOptions.Contains(resolutionString))
-            {
-                resolutionOptions.Add(resolutionString);
-            }
+            sld.value = PlayerPrefs.GetFloat("volume");
+            SliderChange();
         }
-
-        DResolution.AddOptions(resolutionOptions);
-
-        Resolution currentResolution = Screen.currentResolution;
-        string currentResolutionString = $"{currentResolution.width} x {currentResolution.height}";
-        DResolution.value = resolutionOptions.IndexOf(currentResolutionString);
-
-        DResolution.RefreshShownValue();
     }
 }
